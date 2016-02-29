065e3baed08bd1bedd1fabba78059cefaa30ff63
jiimms <jiimms@yahoo.com>
2016-03-01 01:08:35 +0530

    ruby 3.1

---
+++ ruby3.1_get_name.rb
@@ -0,0 +1,16 @@
+
+	def intro
+		puts "Please enter your name"
+		name = gets
+
+		puts "Please enter your age"
+		age = gets
+
+		puts "Your name is #{name}"
+		puts "Your age is #{age}"
+	end
+
+	intro
+	
+	
+		
